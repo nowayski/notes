a94e067f38a3f5d0af4117282bbd65aa7a34cc86
Nick Lane <nickyjl22@gmail.com>
2023-01-11 21:55:37 +0000

    Minor Changes, add login button with no functionality

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,12 @@
-import React, { FC } from "react";
+import React, { FC, MouseEvent } from "react";
 
 const SideBar: FC = () => {
 
+  const motivationButtonHandler = (event: MouseEvent) => {
+    event.preventDefault();
+    alert("Motivation Button clicked")
+  }
+  
   const getMotivationHandler = async (url: string) => {
     const response = await fetch("/api/getMotivation",
     {
@@ -10,13 +15,21 @@ const SideBar: FC = () => {
     })
   }
 
+  const loginHandler = (event: MouseEvent) => {
+    event.preventDefault();
+    
+  }
+
   return (
     <div className="grid grid-cols-1">
       <div>
         <h1 className="font-bold text-xl"><a>Notes</a></h1>
       </div>
       <div>
-        <button onClick={getMotivationHandler} className="my-4 border-2 rounded-lg border-rose-500">Push Me For Motivation.</button>
+        <button onClick={motivationButtonHandler} className="my-4 border-2 rounded-lg border-rose-500">Push Me For Motivation.</button>
+      </div>
+      <div>
+        <button onClick={loginHandler} className="my-4 border-2 border-rose-500 p-1">Login</button>
       </div>
     </div>
   );
